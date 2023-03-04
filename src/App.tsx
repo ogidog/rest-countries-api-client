f2b7461e25d5bfad8485005229f75345812efe08
ogidog <ogidog@mail.ru>
2023-03-05 00:01:47 +0700

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import './App.module.css';
-
-import style from "./App.module.css";
-
-import {useSelector} from "react-redux";
-import {selectThemeMode} from "shared/slices";
-import {RouterProvider} from "react-router-dom";
-import {router} from "shared/router";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    const themeMode = useSelector(selectThemeMode);
-
-    return (
-        <div className={`${style["c-app"]} ${"background_" + themeMode}`}>
-            <RouterProvider router={router}/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
